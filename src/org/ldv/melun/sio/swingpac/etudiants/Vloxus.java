07f11ad8f7aed4b3d510959fdc892bfa560e1112
sio <sio@unassigned-hostname.unassigned-domain>
2013-09-13 11:30:50 +0200

    yo

---
+++ src/org/ldv/melun/sio/swingpac/etudiants/Vloxus.java
@@ -0,0 +1,31 @@
+package org.ldv.melun.sio.swingpac.etudiants;
+
+import java.awt.Color;
+import java.util.List;
+
+import org.ldv.melun.sio.swingpac.Bidule;
+
+public class Vloxus extends Bidule {
+
+ 
+
+  public Vloxus() {
+    super("Vloxus");
+    setBackground(Color.BLACK);
+    this.setSize(50, 50);
+  
+  }
+
+  @Override
+  public void doMove() {
+	    super.doMove();
+	  }
+  public void tuEstouchePar(Bidule biduleImpacteur) {
+	    
+	    
+	    
+  }
+  
+
+
+}
